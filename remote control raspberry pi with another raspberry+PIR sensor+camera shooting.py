027164aa2ced8d4c921a1767e52298226f676ae7
eddy8613 <eddy8613@gmail.com>
2018-06-28 01:29:36 +0800

    Update remote control raspberry pi with another raspberry+PIR sensor+camera shooting.py

--- remote control raspberry pi with another raspberry+PIR sensor+camera shooting.py
+++ remote control raspberry pi with another raspberry+PIR sensor+camera shooting.py
@@ -1,16 +1,17 @@
+#與另一個程式碼相同的部分就不再重複註解
 from time import sleep
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 
-from gpiozero import LED
-from gpiozero.pins.pigpio import PiGPIOFactory
-
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM) #GPIO.BCM 選項是指定GPIO後面的號碼
+GPIO.setmode(GPIO.BCM) 
 GPIO.setup(17, GPIO.IN)
 
-factory = PiGPIOFactory(host='192.168.0.101')
-led = LED(4, pin_factory=factory)
+#remote control 特別需要的library及設定
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
+factory = PiGPIOFactory(host='192.168.0.101') #id位置請查看另一台樹梅派的ip
+led = LED(4, pin_factory=factory) #把LED與GPIO 4連接
 
 
 
@@ -22,7 +23,7 @@ while True:
         camera.capture('/home/pi/Desktop/image.jpg')
         camera.close()
         print("camera shot down and blink a led on another raspberry pi")
-        led.on()
+        led.on() #當裝有PIR sensor的樹莓派接收到紅外線的變化後，另一台樹莓派的LED燈就亮了
         sleep(5)
         led.off()
         print("exit the loop and wait for next IR change")
